bf1b4a9e1b51948a0109d1a94e058eb6fe93d321
Victor Henrique <victorh1705@gmail.com>
2022-12-21 12:38:49 -0300

    style: change app's theme color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color pink = Color(0xffdb28a9);
+const Color purple1 = Color(0xff9d1dca);
+const Color purple2 = Color(0xff892FE0);
+const Color darkGrey = Color(0xffa5a5a5);
+const Color lightGrey = Color(0xff433E48);
